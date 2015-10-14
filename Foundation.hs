ca00068444f29ee7318c5b480ba115cf3299f214
peakysideyou <zazen_shonen@yahoo.co.jp>
2015-10-14 23:25:56 +0900

    変更ファイル追加

--- Foundation.hs
+++ Foundation.hs
@@ -6,6 +6,7 @@ import Text.Jasmine                (minifym)
 import Yesod.Core.Types            (Logger)
 import Yesod.Default.Util          (addStaticContentExternal)
 import qualified Yesod.Core.Unsafe as Unsafe
+import Data.Text
 
 -- | The foundation datatype for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
